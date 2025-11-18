a2d1020cf30587c0c7160a5a11f4958e025ec82a
Ольга Гордеева <olgagordeeva@MacBook-Air-Olga.local>
2025-11-18 21:21:16 +0300

    Обновление

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -3,36 +3,43 @@ let operation = null;
 
 const inputWindow = document.getElementById('inputWindow');
 
-
 document.getElementById('btn_1').addEventListener('click', function () {
-    inputWindow.value +='1';
-})
+    inputWindow.value += '1';
+});
 
 document.getElementById('btn_sum').addEventListener('click', function () {
     lastOperand = parseInt(inputWindow.value);
-    operation ='sum';
+    operation = 'sum';
     inputWindow.value = '';
-})
+});
 
 document.getElementById('btn_def').addEventListener('click', function () {
-    lastOperand - parseInt(inputWindow.value);
-    operation ='def';
+    lastOperand = parseInt(inputWindow.value);
+    operation = 'def';
     inputWindow.value = '';
-})
+});
 
 document.getElementById('btn_calc').addEventListener('click', function () {
-    if (operation === 'def'){
-        const result = lastOperand + parseInt(inputWindow.value);
-        operation = null;
-        lastOperand = 0;
-        inputWindow.value = result;
-    } 
-})
+    const currentValue = parseInt(inputWindow.value);
+    let result;
+
+    if (operation === 'sum') {
+        result = lastOperand + currentValue;
+    } else if (operation === 'def') {
+        result = lastOperand - currentValue;
+    } else {
+        result = currentValue;
+    }
+
+    operation = null;
+    lastOperand = 0;
+    inputWindow.value = result;
+});
 
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '';
-})
+});
 
 
